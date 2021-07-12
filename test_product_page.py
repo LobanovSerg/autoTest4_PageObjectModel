66c4542ff64565407deb26e8bca1bc850b4da8c8
LobanovSerg <north.svd@gmail.com>
2021-07-12 13:12:46 +0300

    Add parametrize and list promo -> test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,13 @@
 from .pages.product_page import ProductPage
+import pytest
 
-link = 'http://selenium1py.pythonanywhere.com/\
-catalogue/the-shellcoders-handbook_209/?promo=newYear'
+link_ = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+promo_list = [f'{link_}?promo=offer{i}' for i in range(10)]
+promo_list[7] = pytest.param(promo_list[7], marks=pytest.mark.xfail)
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', promo_list)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.product_add_to_basket()
